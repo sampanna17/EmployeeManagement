b141bc940d752ad71f038a77ad62638e6d2b0c9b
sampanna17 <sampannapiya13@gmail.com>
2026-06-24 16:41:48 +0545

    User Repo Creation

--- src/main/java/com/springproject/repository/UserRepository.java
+++ src/main/java/com/springproject/repository/UserRepository.java
@@ -1,4 +1,16 @@
 package com.springproject.repository;
 
-public class UserRepository {
+import com.springproject.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository
+        extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
